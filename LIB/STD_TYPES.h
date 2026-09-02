0b3379fe95e69dd4430ef2551a76282fd1ad0478
Sarah Mohamed <saramohamedw2006@gmail.com>
2026-09-02 13:47:10 +0300

    Update STD_TYPES.h

---
+++ LIB/STD_TYPES.h
@@ -0,0 +1,21 @@
+#ifndef _STD_TYPES_H_
+#define _STD_TYPES_H_
+
+typedef unsigned char       u8;
+typedef unsigned short      u16;
+typedef unsigned long       u32;
+typedef unsigned long long  u64;
+
+typedef signed char         s8;
+typedef signed short        s16;
+typedef signed long         s32;
+typedef signed long long    s64;
+
+typedef float                f32;
+typedef double               f64;
+
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
+#endif
